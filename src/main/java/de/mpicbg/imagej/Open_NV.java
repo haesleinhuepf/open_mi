f04e8860c250f6200bac6393eab7971503152f3d
haesleinhuepf <rhaase@mpi-cbg.de>
2019-07-19 22:08:17 -0400

    WIP: Reading image header correctly (pixel type, image width/height/depth)

--- src/main/java/de/mpicbg/imagej/Open_NV.java
+++ src/main/java/de/mpicbg/imagej/Open_NV.java
@@ -17,6 +17,8 @@ import ij.measure.*;
 
 public class Open_NV implements PlugIn
 {
+    ArrayList<HashMap<String, Object>> headerProperties = null;
+
     FileInfo nFileInfo;
     FileOpener nFileOpener;
     ImagePlus nImage;
@@ -35,7 +37,6 @@ public class Open_NV implements PlugIn
 
     public void run(String arg)
     {
-
         //IJ.log("Entering Plugin");
         if (arg.equals(""))
         {
@@ -60,6 +61,8 @@ public class Open_NV implements PlugIn
 
     public void openNV(File fileName)
     {
+        headerProperties = new ArrayList<HashMap<String, Object>>();
+
         //IJ.log("Entering openStp");
 
         //int buffers=0;
@@ -92,7 +95,7 @@ public class Open_NV implements PlugIn
         }
 
         //Set up the images
-        for(int i = 0; i < bufferCount; i++)
+        /*for(int i = 0; i < bufferCount; i++)
         {
 
             //Set Parameters and open each buffer
@@ -109,9 +112,53 @@ public class Open_NV implements PlugIn
             nImage = nFileOpener.open(true);
 
         }	//end for
+        */
+
+        int counter = 0;
+        for (HashMap<String, Object> header : headerProperties) {
+            counter++;
+            System.out.println("Loading " + fileName + " [" + counter + "]");
+            //Set Parameters and open each buffer
+            nFileInfo = new FileInfo();
+            int bytesPerPixel = (int)header.get("bytesperpixel");
+            switch (bytesPerPixel) {
+                case 1:
+                    nFileInfo.fileType = FileInfo.GRAY8;
+                    break;
+                case 4:
+                    nFileInfo.fileType = FileInfo.GRAY32_FLOAT;
+                    break;
+                case 2:
+                default:
+                    nFileInfo.fileType = FileInfo.GRAY16_UNSIGNED;
+                    break;
+            }
+            nFileInfo.fileName = fileName.toString();
+            nFileInfo.width  = (int)header.get("width");
+            if (header.containsKey("height")) {
+                nFileInfo.height = (int) header.get("height");
+            } else if (header.containsKey("data_length")) {
+                nFileInfo.height =  (int)header.get("data_length") / nFileInfo.width;
+            }
+            if (header.containsKey("depth")) {
+                nFileInfo.nImages = (int)header.get("depth");
+            } else {
+                nFileInfo.nImages = (int)header.get("data_length") / nFileInfo.width / nFileInfo.height;
+            }
+
+            nFileInfo.offset = (int)header.get("data_offset");
+            nFileInfo.intelByteOrder = true;
+
+            //Open the Image
+            nFileOpener = new FileOpener(nFileInfo);
+            nImage = nFileOpener.open(true);
+
+            if (header.containsKey("title")) {
+                nImage.setTitle((String)header.get("title"));
+            }
+        }
 
     }	//end openNV
-
 
     public void readHeader(BufferedReader in)
     {
@@ -137,49 +184,108 @@ public class Open_NV implements PlugIn
 
             //Get the buffer info
             //Get the header size
-            if(buf.startsWith("Data length:") && bufferCount == 0)
+            if(buf.startsWith("Data length:")) // && bufferCount == 0)
             {
+                addImageProperty("data_length", new Integer(buf.substring(colonIndex + 1).trim()));
+
                 //IJ.log(new Double(buf.substring(colonIndex + 1).trim()).toString());
                 nVProps[0] = new Double(buf.substring(colonIndex + 1).trim()).doubleValue();
-                IJ.log("Header Length " + new Integer((int) nVProps[0]).toString());
+                IJ.log("Data Length " + (int) nVProps[0]);
                 //bufferCount++;
                 continue;
-            }
+            } else
 
-            //Get the number if pixels
-            if (buf.startsWith("Lines:") && bufferCount == 0)
+            if (buf.startsWith("Samps/line: ")) {
+                String[] temp = buf.substring(colonIndex + 1).trim().split(" ");
+                addImageProperty("width", new Integer(temp[0]));
+                //if (temp.length > 1) {
+                //    addImageProperty("depth", new Integer(temp[1]));
+                //}
+
+
+
+
+
+            } else
+
+
+            if (buf.startsWith("Bytes/pixel: ")) {
+                addImageProperty("bytesperpixel", new Integer(buf.substring(colonIndex + 1).trim()));
+            } else
+
+            if (buf.startsWith("Number of lines: ")) {
+                addImageProperty("height", new Integer(buf.substring(colonIndex + 1).trim()));
+            } else
+
+            if (buf.startsWith("@2:Image Data: ")) {
+                String[] temp = buf.split(":");
+                addImageProperty("title", temp[temp.length - 1].trim());
+            } else
+
+
+
+            //Get the number of pixels
+            if (buf.startsWith("Lines:")) // && bufferCount == 0)
             {
+                addImageProperty("height", new Integer(buf.substring(colonIndex + 1).trim()));
                 nVProps[1] = new Double(buf.substring(colonIndex + 1).trim()).doubleValue();
-                IJ.log("Pixels " + new Integer((int) nVProps[1]).toString());
+                IJ.log("Lines " + (int) nVProps[1]);
                 continue;
-            }
+            } else
 
-            if (buf.startsWith("Scan size:") && bufferCount == 0)
+
+
+            if (buf.startsWith("Scan size:")) // && bufferCount == 0)
             {
+                addImageProperty("scan_size", new Double(buf.substring(colonIndex + 1, unitSkip).trim()));
                 nVProps[2] = new Double(buf.substring(colonIndex + 1, unitSkip).trim()).doubleValue();
-                IJ.log("Scan Size " + new Double(nVProps[2]).toString());
+                IJ.log("Scan Size " + nVProps[2]);
                 continue;
-            }
+            } else
 
-            //Get the image size and offset
+                //Get the image size and offset
             if (buf.startsWith("Data offset:"))
             {
+                appendImage();
+                addImageProperty("data_offset", new Integer(buf.substring(colonIndex + 1).trim()));
+
                 buffProps[0][bufferCount] = new Integer(buf.substring(colonIndex + 1).trim()).intValue();
                 bufferCount += 1;
-                IJ.log("Image Offset " + new Integer((int) buffProps[0][bufferCount - 1]).toString());
+                IJ.log("Data Offset " + new Integer((int) buffProps[0][bufferCount - 1]).toString());
                 continue;
-            }
+            } else
             if (buf.startsWith("Data length:") && bufferCount>0)
             {
+                addImageProperty("data_length", new Integer(buf.substring(colonIndex + 1).trim()));
                 buffProps[1][bufferCount - 1] = new Integer(buf.substring(colonIndex + 1).trim()).intValue();
-                IJ.log("Image Sizez " + new Integer((int) buffProps[1][bufferCount - 1]).toString());
+                IJ.log("Data Sizez " +      new Integer((int) buffProps[1][bufferCount - 1]).toString());
                 //bufferCount++;
                 continue;
+            } else {
+                //IJ.log(buf);
             }
 
         }	//end for
 
     }	//end readHeader
+
+    private void addImageProperty(String key, Object value) {
+        if (headerProperties.size() != 0) {
+            HashMap<String, Object> header = headerProperties.get(headerProperties.size() - 1);
+            header.put(key, value);
+            System.out.println("  " + key + " = " + value);
+        }
+    }
+
+    private void appendImage() {
+        System.out.println("NEW IMAGE");
+        headerProperties.add(new HashMap<String, Object>());
+    }
+
+    public static void main(String[] args) {
+        new ImageJ();
+        new Open_NV().openNV(new File("D:/structure/data/mt_naked_.0_00000.spm"));
+    }
 
 
 }	//end class
